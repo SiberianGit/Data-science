131fbdced389162663e7f07de6c407050a1c02b5
Pavel <pavel.pz@mail.ru>
2022-10-29 17:21:48 +0500

    "Initial commit"

--- game.py
+++ game.py
@@ -1,5 +1,21 @@
-a='Pavel'
-b='Alex'
-c='Artem'
-text=f'{c} Puzanov, {a} Stepnov, {b} Petrov'
-print(text)
+"""Игра угадай число"""
+
+import numpy as np
+
+number = np.random.randint(1, 101) # загадываем число
+count = 0
+
+while True:
+    count += 1
+    predict_number = int(input("Угадай число от 1 до 100: "))
+
+    if predict_number > number:
+        print("Число должно быть меньше!")
+
+    elif predict_number < number:
+        print("Число должно быть больше!")
+
+    else:
+        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        break # конец игры, выход из цикла
+    
